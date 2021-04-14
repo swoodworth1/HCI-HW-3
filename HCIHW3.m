b3da9b96331ee2bcfa787fc0f4dcccbe8ddaa799
swoodworth1 <70487369+swoodworth1@users.noreply.github.com>
2021-04-14 18:51:32 -0400

    Update HCIHW3.m

--- HCIHW3.m
+++ HCIHW3.m
@@ -4,3 +4,15 @@
 % Last Modified: 04/14/2021
 
 clear, clc
+load('commandNet.mat')
+
+[x,fs] = audioread('stop_command.flac');
+sound(x,fs)
+auditorySpect = helperExtractAuditoryFeatures(x,fs);
+command = classify(trainedNet,auditorySpect)
+
+x = audioread('play_command.flac');
+sound(x,fs)
+auditorySpect = helperExtractAuditoryFeatures(x,fs);
+command = classify(trainedNet,auditorySpect)
+
