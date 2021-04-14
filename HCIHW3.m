f630975ac8e96482009455e2af788862bf9faf50
swoodworth1 <70487369+swoodworth1@users.noreply.github.com>
2021-04-14 19:20:31 -0400

    Update HCIHW3.m

--- HCIHW3.m
+++ HCIHW3.m
@@ -6,15 +6,6 @@
 clear, clc
 load('commandNet.mat')
 
-% [x,fs] = audioread('stop_command.flac');
-% sound(x,fs)
-% auditorySpect = helperExtractAuditoryFeatures(x,fs);
-% command = classify(trainedNet,auditorySpect)
-% 
-% x = audioread('play_command.flac');
-% sound(x,fs)
-% auditorySpect = helperExtractAuditoryFeatures(x,fs);
-% command = classify(trainedNet,auditorySpect)
 fs = 16000;
 classificationRate = 20;
 adr = audioDeviceReader('SampleRate',fs,'SamplesPerFrame',floor(fs/classificationRate));
@@ -24,57 +15,105 @@ labels = trainedNet.Layers(end).Classes;
 YBuffer(1:classificationRate/2) = categorical("background");
 
 probBuffer = zeros([numel(labels),classificationRate/2]);
-
+tts('Specify face location');
 countThreshold = ceil(classificationRate*0.2);
 probThreshold = 0.7;
 
-h = figure('Units','normalized','Position',[0.2 0.1 0.6 0.8]);
+word1 = 0;
+word2 = 0;
 
-timeLimit = 20;
+while word1 == 0
 
-tic
-while ishandle(h) && toc < timeLimit
-
-    % Extract audio samples from the audio device and add the samples to
-    % the buffer.
     x = adr();
     write(audioBuffer,x);
     y = read(audioBuffer,fs,fs-adr.SamplesPerFrame);
 
     spec = helperExtractAuditoryFeatures(y,fs);
 
-    % Classify the current spectrogram, save the label to the label buffer,
-    % and save the predicted probabilities to the probability buffer.
     [YPredicted,probs] = classify(trainedNet,spec,'ExecutionEnvironment','cpu');
     YBuffer = [YBuffer(2:end),YPredicted];
     probBuffer = [probBuffer(:,2:end),probs(:)];
 
-    % Plot the current waveform and spectrogram.
-    subplot(2,1,1)
-    plot(y)
-    axis tight
-    ylim([-1,1])
-
-    subplot(2,1,2)
-    pcolor(spec')
-    caxis([-4 2.6445])
-    shading flat
-
-    % Now do the actual command detection by performing a very simple
-    % thresholding operation. Declare a detection and display it in the
-    % figure title if all of the following hold: 1) The most common label
-    % is not background. 2) At least countThreshold of the latest frame
-    % labels agree. 3) The maximum probability of the predicted label is at
-    % least probThreshold. Otherwise, do not declare a detection.
-    [YMode,count] = mode(YBuffer);
-
-    maxProb = max(probBuffer(labels == YMode,:));
-    subplot(2,1,1)
-    if YMode == "background" || count < countThreshold || maxProb < probThreshold
-        title(" ")
-    else
-        title(string(YMode),'FontSize',20)
+    [word,count] = mode(YBuffer);
+    maxProb = max(probBuffer(labels == word,:));
+    
+    if (word ~= "down" && word ~= "up")
+        
+    else 
+        word
+        word1 = 1;
     end
-
+    
     drawnow
 end
+
+while word2 == 0
+
+    x = adr();
+    write(audioBuffer,x);
+    y = read(audioBuffer,fs,fs-adr.SamplesPerFrame);
+
+    spec = helperExtractAuditoryFeatures(y,fs);
+
+    [YPredicted,probs] = classify(trainedNet,spec,'ExecutionEnvironment','cpu');
+    YBuffer = [YBuffer(2:end),YPredicted];
+    probBuffer = [probBuffer(:,2:end),probs(:)];
+
+    [word,count] = mode(YBuffer);
+    word;
+    maxProb = max(probBuffer(labels == word,:));
+    
+    if (word ~= "left" && word ~= "right")
+
+    else
+        word
+        word2 = 1;
+    end
+    
+    drawnow
+end
+
+
+function wav = tts(txt,voice,pace,fs)
+if ~ispc, error('Microsoft Win32 SAPI is required.'); end
+if ~ischar(txt), error('First input must be string.'); end
+SV = actxserver('SAPI.SpVoice');
+TK = invoke(SV,'GetVoices');
+if nargin > 1
+    % Select voice;
+    for k = 0:TK.Count-1
+        if strcmpi(voice,TK.Item(k).GetDescription)
+            SV.Voice = TK.Item(k);
+            break;
+        elseif strcmpi(voice,'list')
+            disp(TK.Item(k).GetDescription);
+        end
+    end
+    % Set pace;
+    if nargin > 2
+        if isempty(pace), pace = 0; end
+        if abs(pace) > 10, pace = sign(pace)*10; end        
+        SV.Rate = pace;
+    end
+end
+if nargin < 4 || ~ismember(fs,[8000,11025,12000,16000,22050,24000,32000,...
+        44100,48000]), fs = 16000; end
+if nargout > 0
+   % Output variable;
+   MS = actxserver('SAPI.SpMemoryStream');
+   MS.Format.Type = sprintf('SAFT%dkHz16BitMono',fix(fs/1000));
+   SV.AudioOutputStream = MS;  
+end
+invoke(SV,'Speak',txt);
+if nargout > 0
+    % Convert uint8 to double precision;
+    wav = reshape(double(invoke(MS,'GetData')),2,[])';
+    wav = (wav(:,2)*256+wav(:,1))/32768;
+    wav(wav >= 1) = wav(wav >= 1)-2;
+    delete(MS);
+    clear MS;
+end
+delete(SV); 
+clear SV TK;
+pause(0.2);
+end;
